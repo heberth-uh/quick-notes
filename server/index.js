18fa67aa985efad2ed52177cb153fe35ec5c7ba8
heberthuh <heberth.uh@tohsoluciones.com>
2024-09-13 00:35:32 -0600

    Watching /client/dist in backend

--- server/index.js
+++ server/index.js
@@ -9,14 +9,15 @@ import taskRoutes from "./routes/tasks.routes.js";
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
-app.use(cors({ origin: FRONTEND_URL }));
+// app.use(cors({ origin: FRONTEND_URL }));
+app.use(cors());
 app.use(express.json());
 
 app.use(routes);
 app.use(taskRoutes);
 
 // Distribución de archivos estáticos (client built)
-// app.use(express.static(join(__dirname, '../client/dist')))
+app.use(express.static(join(__dirname, '../client/dist')))
 
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
